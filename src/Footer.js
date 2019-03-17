7c91683d749d08aadba52ba757ab8ec623cc34b3
Viachaslau Tsimoshchanka <viachaslau_tsimoshchanka@epam.com>
2019-03-17 21:11:01 +0300

    task1: add components

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+// Functional Component
+
+import React from 'react';
+import './Footer.css';
+
+export const Footer = () =>(
+    <footer>
+        <div className="logo">NetflixRoulette</div>
+    </footer>
+); 
